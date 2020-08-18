7740a9d2a0f4c7a38f97145916230ff7df49e89a
BackinR2D2 <cristimlg334@gmail.com>
2020-08-18 21:27:57 +0300

    authorization bug fix

--- routes/checkAuth.js
+++ routes/checkAuth.js
@@ -5,14 +5,14 @@ module.exports = function (req, res, next) {
         const situation = jwt.decode(req.cookies.token)
         if (situation == null) return res.redirect('/login')
         if (Date.now() >= situation.exp * 1000) {
-            return res.status(401).json({ msg: 'Token expired. Login again.' })
+            return res.status(401).redirect('/login')
         }
         if (!req.cookies.token) {
-            return res.status(403).json({ msg: 'Login required.' })
+            return res.status(403).redirect('/login')
         } else if (req.cookies.token) {
             return next()
         }
     } catch (error) {
-        res.status(400).json({ msg: 'Some error occured. I am sorry!' })
+        res.status(400).redirect('/login')
     }
 }
